3d0ab24d937901f508bab3dbaf5ff0b6f734b92f
Jonathan Pool <pool@jpdev.pro>
2026-07-19 15:19:39 -0700

    debug

--- src/requestHandler.ts
+++ src/requestHandler.ts
@@ -10,10 +10,15 @@ import {handleError, log} from './util.ts';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// Route map: URL path → HTML file
+// Route map: URL path → file
 export const routes: Record<string, string> = {
   '/': 'tutorial.html',
-  '/comments': 'comments.html'
+  '/comments': 'comments.html',
+  '/favicon.ico': 'favicon.ico'
+};
+const contentTypeMap: Record<string, string> = {
+  'html': 'text/html',
+  'ico': 'image/x-icon'
 };
 
 // FUNCTIONS
@@ -27,13 +32,21 @@ export const handler = async (req: IncomingMessage, res: ServerResponse) => {
     return;
   }
   try {
-    // Get the HTML file content.
-    const content = await readFile(join(__dirname, '..', 'public', file), 'utf-8');
+    // Get the file content type.
+    const contentType = contentTypeMap[file.split('.').pop()!] || '';
+    // If this failed:
+    if (! contentType) {
+      handleError(res, `Server failed to serve ${url} (${file}) because its content type is unknown`, 500);
+      return;
+    }
+    // Otherwise, i.e. if it succeeded, get the file content.
+    const contentBuffer = await readFile(join(__dirname, '..', 'public', file));
     // Serve it to the client.
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(content);
+    res.writeHead(200, {'Content-Type': contentType});
+    res.end(contentBuffer);
     log('info', 'response', file, 200);
-  } catch (_) {
+  }
+  catch {
     handleError(res, `Server failed to serve ${url} (${file})`, 500);
   }
 }
